c36324580b38c1e7b5f674d36658b2ada7e99619
BenK <ebaykazemi@googlemail.com>
2016-04-26 13:08:46 +0100

    Description added

--- newBenscript_V7.1.py
+++ newBenscript_V7.1.py
@@ -4,6 +4,9 @@
 # 	Copyright (C) 2015 - 2016:
 #          Ben Kazemi, ebaykazemi@googlemail.com
 
+# This script simplifies communications with a 16 bit addressed big endian, 1-32 byte little endian data IO device.
+
+
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 3
